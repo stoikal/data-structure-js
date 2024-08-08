a7ada3377021cde2fbabe4b1a59722e461e4c83d
Laksito Pamilih <laksito@protonmail.com>
2024-08-08 14:03:35 +0700

    Stack

---
+++ Stack/index.js
@@ -0,0 +1,47 @@
+import LinkedList from "../SinglyLinkedList";
+
+class Stack {
+  constructor(maxSize = Infinity) {
+    this.stack = new LinkedList();
+    this.maxSize = maxSize;
+    this.size = 0;
+  }
+
+  hasRoom() {
+    return this.size < this.maxSize;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  push(value) {
+    if (this.hasRoom()) {
+      this.stack.addToHead(value);
+      this.size++;
+    } else {
+      throw new Error("Stack is full");
+    }
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      console.log('Stack is empty!');
+    } else {
+      const value = this.stack.removeHead();
+      this.size--;
+      return value;
+    }
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      return this.stack.head.data;
+    }
+  }
+
+}
+
+export default Stack;
