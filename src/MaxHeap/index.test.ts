630866613613b31e2d137557a3ad8a049842f72e
Laksito Pamilih <laksito@protonmail.com>
2024-09-06 17:00:47 +0700

    add tests for MaxHeap and MinHeap

--- src/MaxHeap/index.test.ts
+++ src/MaxHeap/index.test.ts
@@ -1,16 +1,28 @@
-import MaxHeap from "./index";
+import {expect, test} from '@jest/globals'
+import MaxHeap from "./index"
 
-test('popMax', () => {
+test('size', () => {
+  const SIZE = 50;
   const mh = new MaxHeap()
 
-  for (let i = 1; i <= 50; i++) {
-    mh.add(i);
+  for (let i = 1; i <= SIZE; i++) {
+    mh.add(i)
   }
 
-  for (let i = 50; i < 0; i--) {
+  expect(mh.size).toBe(SIZE)
+})
+
+test('popMax', () => {
+  const SIZE = 50;
+  const mh = new MaxHeap()
+
+  for (let i = 1; i <= SIZE; i++) {
+    mh.add(i)
+  }
+
+  for (let i = SIZE; i < 0; i--) {
     const max = mh.popMax()
-  
-    console.log("max", max)
+
     expect(max).toBe(i)
   }
-});
+})
--- src/MinHeap/index.test.ts
+++ src/MinHeap/index.test.ts
@@ -1,16 +1,28 @@
-import MinHeap from "./index";
+import {expect, test} from '@jest/globals'
+import MinHeap from "./index"
 
-test('popMin', () => {
+test('size', () => {
+  const SIZE = 50;
   const mh = new MinHeap()
 
-  for (let i = 1; i <= 50; i++) {
-    mh.add(i);
+  for (let i = 1; i <= SIZE; i++) {
+    mh.add(i)
   }
 
-  for (let i = 1; i <= 0; i++) {
-    const max = mh.popMin()
-  
-    console.log("max", max)
-    expect(max).toBe(i)
-  }
+  expect(mh.size).toBe(SIZE)
 });
+
+test('popMin', () => {
+  const SIZE = 50;
+  const mh = new MinHeap()
+
+  for (let i = 1; i <= SIZE; i++) {
+    mh.add(i)
+  }
+
+  for (let i = 50; i < 0; i--) {
+    const min = mh.popMin()
+    console.log(i)
+    expect(min).toBe(i)
+  }
+})
